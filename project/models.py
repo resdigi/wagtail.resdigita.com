f1004765baae30a07866984d722885f05db08f0e
Chris Mann <chris@lesgtrandsvoisins.com>
2025-02-19 15:09:34 +0100

    Changed model of Project

--- project/models.py
+++ project/models.py
@@ -2,20 +2,38 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.search import index
-
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 
 class ProjectIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + ["intro"]
 
+class ProjectPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ProjectPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
 class ProjectPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=ProjectPageTag, blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
-    content_panels = Page.content_panels + ["intro", "body"]
+    content_panels = Page.content_panels + ["tags", "intro", "image", "body"]
