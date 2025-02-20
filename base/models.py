4a53a3980f3502c9d87b64909684dfdb4a39197b
Sviatlana Viarbitskaya <sviatlana.viarbitskaya@gmail.com>
2025-02-20 12:03:48 +0100

    added contact page model and tempaltes

--- base/models.py
+++ base/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from wagtail.admin.panels import (
     FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
     MultiFieldPanel,
     PublishingPanel,
 )
@@ -19,10 +21,11 @@ from wagtail.contrib.settings.models import (
     register_setting,
 )
 
-# import register_snippet:
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 from wagtail.snippets.models import register_snippet
-
 from modelcluster.fields import ParentalKey
+
 
 @register_setting
 class NavigationSettings(BaseGenericSetting):
@@ -67,3 +70,26 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer Text"
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
+
