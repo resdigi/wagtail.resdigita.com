0d6347a683f579b6942cea86c7b99b5695affbf6
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-12 15:18:06 +0200

    Fixed Tailwind in wrong places

--- resdigita/tailwind.config.js
+++ resdigita/tailwind.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
 module.exports = {
     // content: [
-    //   './**'
-    //   // './src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
-    //   // './node_modules/astro-boilerplate-components/**/*.{js,ts,jsx,tsx}',
+    //     './templates/**'
+    //     //   './**'
+    //     //   // './src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+    //     //   // './node_modules/astro-boilerplate-components/**/*.{js,ts,jsx,tsx}',
     // ],
     theme: {
         extend: {},
